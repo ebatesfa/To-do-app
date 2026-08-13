b054703c49dc8a64364e9092b8ffd01f764c7fce
ebatesfa <ebatesfa9@gmail.com>
2026-08-13 10:11:09 -0700

    add delete task functionality

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 console.log("JS is working");
 
-let input= document.getElementById("textinput");
-let addBtn=document.getElementById("addBtn");
+let input = document.getElementById("textinput");
+let addBtn = document.getElementById("addBtn");
 
-addBtn.addEventListener("click", function addTask(){
- const task= input.value;
- const li = document.createElement("li");
- li.textContent=task;
- const list =document.querySelector("ul");
-list.appendChild(li);
-input.value = ""; 
+addBtn.addEventListener("click", function addTask() {
+    const task = input.value;
+    if (task.trim() === "") {
+    return;
+}
 
+    const li = document.createElement("li");
+    li.textContent = task;
 
+    const list = document.querySelector("ul");
+    list.appendChild(li);
 
+    input.value = "";
 
-})
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "DELETE";
+
+    li.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", function() {
+        li.remove();
+    });
+});
